ebbce3b02b8854a877d3a3d01269da51a6a94ad4
hoanga4bg <hoanga4bg@gmail.com>
2021-10-31 18:18:00 +0700

    1

---
+++ src/main/java/com/music/UploaddemoApplication.java
@@ -0,0 +1,13 @@
+package com.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploaddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/UploaddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
